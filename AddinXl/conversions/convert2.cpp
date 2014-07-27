d7585c50ad6eb1163b6c5855a3b688fbc080e8d9
Eric Ehlers <eric.ehlers@nazcatech.be>
2014-07-27 18:26:24 +0200

    consolidate conversion code

--- AddinXl/conversions/convert2.cpp
+++ AddinXl/conversions/convert2.cpp
@@ -1,39 +1,8 @@
 
 #include "convert2.hpp"
-#include <boost/lexical_cast.hpp>
+#include "qlo/conversions/cnv_tmpl.hpp"
 
 namespace ObjectHandler {
-
-    // FIXME consolidate with AddinObjects/conversions/convert2.cpp
-
-    inline bool is_numeric(const std::string &s, double &d) {
-        try {
-            d = boost::lexical_cast<double>(s);
-            return true;
-        } catch(...) {
-            return false;
-        }
-    }
-
-    template<class container_t>
-    QuantLib::Date convertDate(const container_t& c) {
-        if(c.type() == typeid(long))
-            return QuantLib::Date(c.operator long());
-        //else if(c.type() == typeid(double))
-        //    return QuantLib::Date(static_cast<QuantLib::BigInteger>(c.operator double()));
-        else if(c.type() == typeid(std::string)) {
-            std::string s = c.operator std::string();
-            double d;
-            if (is_numeric(s, d))
-                return QuantLib::Date(static_cast<QuantLib::BigInteger>(d));
-        //    if (QuantLib::IMM::isIMMcode(s, false))
-        //        return QuantLib::IMM::date(s);
-        //    QuantLib::Period p = QuantLib::PeriodParser::parse(s);
-        //    QuantLib::Date d2 = QuantLib::Settings::instance().evaluationDate();
-        //    return d2 + p;
-        }
-        OH_FAIL("unable to convert type '" << c.type().name() << "' to type 'QuantLib::Date'");
-    }
 
     template<> 
     QuantLib::Date convert2<QuantLib::Date, ConvertOper>(const ConvertOper& c) {
---
+++ qlo/conversions/cnv_tmpl.hpp
@@ -0,0 +1,41 @@
+
+#ifndef addinobjects_conversions_cnv_tmpl_hpp
+#define addinobjects_conversions_cnv_tmpl_hpp
+
+#include <ql/time/date.hpp>
+#include <boost/lexical_cast.hpp>
+
+namespace ObjectHandler {
+
+    inline bool is_numeric(const std::string &s, double &d) {
+        try {
+            d = boost::lexical_cast<double>(s);
+            return true;
+        } catch(...) {
+            return false;
+        }
+    }
+
+    template<class container_t>
+    QuantLib::Date convertDate(const container_t& c) {
+        if(c.type() == typeid(long))
+            return QuantLib::Date(c.operator long());
+        //else if(c.type() == typeid(double))
+        //    return QuantLib::Date(static_cast<QuantLib::BigInteger>(c.operator double()));
+        else if(c.type() == typeid(std::string)) {
+            std::string s = c.operator std::string();
+            double d;
+            if (is_numeric(s, d))
+                return QuantLib::Date(static_cast<QuantLib::BigInteger>(d));
+        //    if (QuantLib::IMM::isIMMcode(s, false))
+        //        return QuantLib::IMM::date(s);
+        //    QuantLib::Period p = QuantLib::PeriodParser::parse(s);
+        //    QuantLib::Date d2 = QuantLib::Settings::instance().evaluationDate();
+        //    return d2 + p;
+        }
+        OH_FAIL("unable to convert type '" << c.type().name() << "' to type 'QuantLib::Date'");
+    }
+}
+
+#endif
+

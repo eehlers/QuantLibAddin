7dfa3bef33e16d745d891bb96538ee2ea1af49e8
Eric Ehlers <eric.ehlers@reposit.be>
2015-11-27 23:14:38 +0100

    fix bug in spreadsheet

--- Clients/CountifyExcelLocal/GBP_Market.bat
+++ Clients/CountifyExcelLocal/GBP_Market.bat
@@ -1,5 +1,5 @@
 
 SET XLL_PATH=..\..\..\QuantLibXL2\xll\QuantLibXL-vc90-mt-s-1_7_0.xll
-@REM SET XLS_PATH=.\GBP_Market.xlsx
+@REM SET XLL_PATH=..\..\..\QuantLibXL2\xll\QuantLibXL-vc90-mt-sgd-1_7_0.xll
 START "EXCEL" "EXCEL.EXE" /e GBP_Market.xlsm
 
